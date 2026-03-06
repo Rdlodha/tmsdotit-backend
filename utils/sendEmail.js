03595e319f832415a47aa6c780f6d692040eec40
Rdlodha <rdlodha007@gmail.com>
2026-03-06 17:22:23 +0530

    email forced ipv4

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -6,11 +6,15 @@ const nodemailer = require("nodemailer");
  */
 async function sendEmail({ to, subject, text, html }) {
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE || "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: false, // use SSL
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        family: 4,
+    
     });
 
     const mailOptions = {
